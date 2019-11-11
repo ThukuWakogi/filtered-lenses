31792b9ed3e9d0d220dd38b2d0c53cabec054997
Timothy Oliver <oliverwakogi@live.com>
2019-11-11 23:53:26 +0300

    Add retrieve all method and respective test

--- gallery/models.py
+++ gallery/models.py
@@ -6,20 +6,30 @@ from django.db import models
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
 
+    def save_location(self): self.save()
+
     def __str__(self): return f'{self.location_name}'
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
 
+    def save_category(self):
+        self.save()
+
     def __str__(self): return f'{self.category_name}'
 
 
 class Image(models.Model):
-    image_content = models.ImageField(upload_to='gallery/')
     name = models.CharField(max_length=30)
     description = models.TextField()
+    url = models.ImageField(upload_to='gallery/')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
+
+    def save_image(self): self.save()
+
+    @classmethod
+    def get_all(cls): return cls.objects.all()
 
     def __str__(self): return f'{self.name}'
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+
+
+class ImageTestClass(TestCase):
+	def setUp(self):
+		self.test_location = Location(location_name='Test Location')
+		self.test_location.save_location()
+		self.test_category = Category(category_name='Test Category')
+		self.test_category.save_category()
+		self.test_image = Image(
+			name='Test image',
+			description='test description',
+			url='gallery/test.jpg',
+			location=self.test_location
+		)
+		self.test_image.save_image()
+		self.test_image.categories.add(self.test_category)
+
+	def tearDown(self):
+		Image.objects.all().delete()
+		Category.objects.all().delete()
+		Location.objects.all().delete()
+
+	def test_get_images(self): self.assertTrue(len(Image.get_all()), 1)
+
