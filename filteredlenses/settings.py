def0f65d75380d1f6b115376963d24b8817c9c61
Timothy Oliver <oliverwakogi@live.com>
2019-11-11 12:33:06 +0300

    create models

--- filteredlenses/settings.py
+++ filteredlenses/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+CITIES_COUNTRY_MODEL = 'gallery.CustomCountryModel'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,8 +79,10 @@ WSGI_APPLICATION = 'filteredlenses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'filtered_lenses',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD')
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    Category_name = models.CharField(max_length=30)
+
+
+class Image(models.Model):
+    image_content = models.ImageField(upload_to='gallery/')
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
